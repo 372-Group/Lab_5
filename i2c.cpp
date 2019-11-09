70388d7430cd4bc0a24a49c77368f57fda1c98b7
AV-Coding <villasenor.ariel@gmail.com>
2019-11-09 15:05:17 -0700

    cpp file

---
+++ i2c.cpp
@@ -0,0 +1,17 @@
+#include <i2c.h>
+
+void beginTransmission(int num){
+
+}
+void endTransmission(){
+
+}
+void read(){
+
+}
+void write(int num){
+
+}
+void requestFrom(int num1, int num2){
+    
+}
