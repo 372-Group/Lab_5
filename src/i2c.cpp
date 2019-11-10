3ce49019533b14301586269795f85157371aba5e
AV-Coding <villasenor.ariel@gmail.com>
2019-11-09 17:05:17 -0700

    begin

--- src/i2c.cpp
+++ src/i2c.cpp
@@ -1,7 +1,10 @@
-#include <i2c.h>
+#include "i2c.h"
+#include <avr/io.h>
+
 #define wait_for_completion while(!(TWCR & (1 << TWINT)));
+
 void beginTransmission(int num){
-    TWCR = ((1 << TWEN) | (1<<TWINT) | (1<<TWINT));
+    TWCR = ((1 << TWEN) | (1<<TWINT) | (1<<TWSTA));
     wait_for_completion;
 }
 void endTransmission(){
