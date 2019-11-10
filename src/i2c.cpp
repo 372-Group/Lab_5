f982fb6d93f34697158c085e563b7ec449dc23a2
AV-Coding <villasenor.ariel@gmail.com>
2019-11-09 17:44:49 -0700

    begin transmission

--- src/i2c.cpp
+++ src/i2c.cpp
@@ -6,14 +6,15 @@
 
 void init2C(){
     TWSR |= (1 << TWPS0);
-    TWSR &= ~(1 << TWPS1);
-    TWBR = 0xC6;
-    TWCR |= (1 << TWINT) | (1 << TWEN);
+    TWSR &= ~(1 << TWPS1); // prescalar = 1
+    TWBR = 0xC6; //bit rate generator = 10k (TWBR = 198)
+    TWCR |= (1 << TWINT) | (1 << TWEN); // enable two wire interface
 }
 
 void beginTransmission(int num){
     TWCR = ((1 << TWEN) | (1<<TWINT) | (1<<TWSTA));
     wait_for_completion;
+    TWDR = num;
 }
 void endTransmission(){
 
