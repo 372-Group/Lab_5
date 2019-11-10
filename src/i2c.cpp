5ba99704335b3abec324321c1d1bd78fceebda76
jjpalanca <jjpalanca@email.arizona.edu>
2019-11-09 17:18:29 -0700

    added initi2c

--- src/i2c.cpp
+++ src/i2c.cpp
@@ -3,6 +3,14 @@
 
 #define wait_for_completion while(!(TWCR & (1 << TWINT)));
 
+
+void init2C(){
+    TWSR |= (1 << TWPS0);
+    TWSR &= ~(1 << TWPS1);
+    TWBR = 0xC6;
+    TWCR |= (1 << TWINT) | (1 << TWEN);
+}
+
 void beginTransmission(int num){
     TWCR = ((1 << TWEN) | (1<<TWINT) | (1<<TWSTA));
     wait_for_completion;
